509a66a8a5e94dbb3fc73273ff0624e0009306a7
FroniYT <71455164+FroniYT@users.noreply.github.com>
2021-08-19 23:48:34 +0700

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,42 @@
+
+ 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+count = 0
+
+while True:
+    count += 1
+
+    msg = MIMEMultipart()
+    
+    
+    message = f"Сын твари дохлой: {count}"
+    
+
+    password = "gamesense2323"
+    msg['From'] = "prohorov_oleg_pasha_lox@mail.ru"
+    msg['To'] = "froniyt@bk.ru"
+    msg['Subject'] = f"Паша лох{count}"
+    
+
+    msg.attach(MIMEText(message, 'plain'))
+    
+
+    server = smtplib.SMTP('smtp.mail.ru: 25')
+    
+    server.starttls()
+    
+
+    server.login(msg['From'], password)
+    
+    
+
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    
+    server.quit()
+    
+    print(f'send: {count} ')
+
+input()
